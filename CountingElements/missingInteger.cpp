06d1e2b9534decf6b48d4547ab0ec2bf6cafa9d7
Mateo <mateusz.pawlicki@hotmail.com>
2019-03-05 09:32:25 +0100

    implementacja missing integer 55%

--- CountingElements/missingInteger.cpp
+++ CountingElements/missingInteger.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
 
 #include <cassert>
 using namespace std;
 
-int solution(vector<int>& A)
+int solution(vector<int>& A) //55%
 {
-    sort(A.begin(), A.end());
-    for(auto i = 0; i < A.size(); ++i)
+    set<int> asset(A.begin(), A.end());
+    auto rbegin = asset.rbegin();
+    if(*rbegin <=0)
+        return 1;
+    auto index = 1;
+    for(const auto& a : asset)
     {
-
+        if(a != index)
+            return index;
+        ++index;
+        
     }
-
-    return 0;
+    return index;
 }
 
 int main()
@@ -25,7 +32,7 @@ int main()
     assert(4 == solution(arr2));
 
     vector<int> arr3{-1,-3};
-    assert(0 == solution(arr3));
+    assert(1 == solution(arr3));
 
     return 0;
 }
