3245415ef54ae402bcf45fa6d9c22cc65fd1cf57
Mateo <mateusz.pawlicki@hotmail.com>
2019-12-09 20:48:59 +0100

    passingCars 100% :)

--- PrefixSum/passingCars.cpp
+++ PrefixSum/passingCars.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -14,40 +13,21 @@ vector<int> prefixSum(const vector<int>& A)
     return prefSum;
 }
 
-int passedElement(const vector<int>& A)
-{
-    vector<int> P(A.size()+1,0);
-    for(auto i = 1; i < P.size(); ++i)
-    {
-        P[i] = P[i-1] + A[i-1];
-    }
-    /*cout <<"tab passed elem\n";
-     for(const auto& i : P)
-        cout <<i <<',';
-    cout <<endl;
-    cout <<"passedElement: " <<P[P.size()] <<endl;
-    cout <<"size: " <<P.size() <<endl; */
-    return P[P.size() - 1];
-}
-
 int solution(vector<int>& A)
 {
-    int result = 0;
-    auto iterFirst = find(A.begin(), A.end(), 0);
-    auto iterLast = A.end();
-    while(iterFirst != A.end())
+    auto prefSum = prefixSum(A);
+    auto N = A.size() - 1;
+    int total = 0;
+    for(int i = 0; i < A.size(); ++i)
     {
-        if(result > 1000000000)
-            return -1;
-        vector<int> tmp(iterFirst, iterLast);
-        /* for(const auto& i : tmp)
-            cout <<i <<',';
-        cout <<endl; */
-        result += passedElement(tmp);
-        auto newIter = ++iterFirst;
-        iterFirst = find(newIter, iterLast, 0);
+        if(0 == A[i])
+        {
+            total += prefSum[N + 1] - prefSum[i+1];
+            if(total > 1000000000)
+                return -1;
+        }
     }
-    return result;
+    return total;
 }
 
 int main(int argc, char* argv[])
@@ -56,5 +36,7 @@ int main(int argc, char* argv[])
     cout << solution(tmp) <<endl;
     vector<int> tmp2 = {1,0,1,1,1,0,1,1,0};
     cout << solution(tmp2) <<endl;
+    vector<int> tmp3 = {1,0,1,1,0,1};
+    cout << solution(tmp3) <<endl;
     return 0;
 }
