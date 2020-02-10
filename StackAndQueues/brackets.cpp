a13d5750d4f96c18b11cac22265eac9c3b335916
Mateusz Pawlicki <mateusz.pawlicki.ext@nokia.com>
2020-02-10 11:49:43 +0100

    brackets - 100%

--- StackAndQueues/brackets.cpp
+++ StackAndQueues/brackets.cpp
@@ -6,12 +6,48 @@ using namespace std;
 
 int solution(string& S)
 {
+    if(')' == S[0] or '}' == S[0] or ']' == S[0])
+        return 0;
+    if(S.size() % 2 != 0)
+        return 0;
+    stack<char> chars;
+    for(auto c : S)
+    {
+        if('(' == c)
+            chars.push(c);
+        if('[' == c)
+            chars.push(c);
+        if('{' == c)
+            chars.push(c);
+        if(')' == c)
+        {
+            auto s = chars.top();
+            if('(' == s)
+                chars.pop();
+        }
+        if(']' == c)
+        {
+            auto s = chars.top();
+            if('[' == s)
+                chars.pop();
+        }
+        if('}' == c)
+        {
+            auto s = chars.top();
+            if('{' == s)
+                chars.pop();
+        }
+    }
 
-    return 0;
+    return chars.empty() ? 1 : 0;
 }
 
 
 int main()
 {
+    std::string S{"{[()()]}"};
+    std::cout <<solution(S) <<endl;
+    std::string S1( "([)()]");
+    std::cout <<solution(S1) <<endl;
     return 0;
 }
